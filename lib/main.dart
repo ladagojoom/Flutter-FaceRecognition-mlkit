fad943f0f0ac00c5b313c6ca3f979413ccd29538
ladagojoom <poondev2x@gmail.com>
2024-04-25 20:51:15 +0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:face_recognation/face_detector_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    home: FaceDetectorView(),
+  ));
+}
